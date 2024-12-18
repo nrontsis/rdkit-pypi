7929b39ba41ca95952f17a02e32f2fa69437af6f
Nikitas Rontsis <nrontsis@gmail.com>
2024-12-18 15:50:45 +0000

    Add fixes

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM quay.io/pypa/manylinux_2_28_x86_64:latest
 WORKDIR /project
 RUN yum update -y && yum install freetype-devel libpng-devel pixman-devel zlib-devel eigen3-devel cairo-devel -y
-ENV PATH=$PATH:/opt/python/cp311-cp311/bin
+ENV ORIGINAL_PATH=$PATH
+ENV PATH=$ORIGINAL_PATH:/opt/python/cp311-cp311/bin
 ENV CIBW_BUILD=cp311-manylinux_x86_64
+RUN python -m build --wheel || python -m build --wheel
--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-rm -rf wheelhouse/*
-CIBW_BUILD=cp310-manylinux_x86_64 python3 -m cibuildwheel --platform linux --output-dir wheelhouse --config-file pyproject.toml
 CIBW_BUILD=cp311-manylinux_x86_64 python3 -m cibuildwheel --platform linux --output-dir wheelhouse --config-file pyproject.toml
+CIBW_BUILD=cp310-manylinux_x86_64 python3 -m cibuildwheel --platform linux --output-dir wheelhouse --config-file pyproject.toml
 CIBW_BUILD=cp312-manylinux_x86_64 python3 -m cibuildwheel --platform linux --output-dir wheelhouse --config-file pyproject.toml
